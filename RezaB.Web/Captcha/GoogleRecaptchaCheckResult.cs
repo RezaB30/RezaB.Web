94539c2130d6d841db758659f288efa88fc8de32
RezaB30 <r.barzegaran@gmail.com>
2021-01-30 18:26:47 +0300

    V 1.1.2 - Added google recaptcha.

---
+++ RezaB.Web/Captcha/GoogleRecaptchaCheckResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezaB.Web.Captcha
+{
+    class GoogleRecaptchaCheckResult
+    {
+        public bool Success { get; set; }
+        public List<string> ErrorCodes { get; set; }
+    }
+}
---
+++ RezaB.Web/Captcha/GoogleRecaptchaValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RezaB.Web.Captcha
+{
+    public static class GoogleRecaptchaValidator
+    {
+        public static GoogleRecaptchaResultType Check(string apiKey, string requestKey)
+        {
+            
+            if (string.IsNullOrWhiteSpace(apiKey) || string.IsNullOrWhiteSpace(requestKey))
+            {
+                return GoogleRecaptchaResultType.NotWorking;
+            }
+
+            try
+            {
+                var client = new System.Net.WebClient();
+                var googleReply = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", apiKey, requestKey));
+                var checkResult = JsonConvert.DeserializeObject<GoogleRecaptchaCheckResult>(googleReply);
+
+                return checkResult.Success ? GoogleRecaptchaResultType.Success : GoogleRecaptchaResultType.Fail;
+            }
+            catch
+            {
+                return GoogleRecaptchaResultType.NotWorking;
+            }
+        }
+    }
+}
